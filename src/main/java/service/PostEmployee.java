63b1cc7ee793ce076a355659f31cbd5349872b9e
P.Mirzapour <pooyamirzapour@yahoo.com>
2021-04-26 11:41:42 +0430

    okhttp example

--- src/main/java/service/PostEmployee.java
+++ src/main/java/service/PostEmployee.java
@@ -3,6 +3,8 @@ package service;
 import okhttp3.*;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PostEmployee {
 
@@ -13,14 +15,17 @@ public class PostEmployee {
 //                .add("password", "test")
                 .build();
 
+        Map<String , String> header = new HashMap<String, String>();
+        Headers headerbuild = Headers.of(header);
         Request request = new Request.Builder()
                 .url("http://dummy.restapiexample.com/api/v1/create")
                 .post(formBody)
+                .headers(headerbuild)
                 .build();
         OkHttpClient client = new OkHttpClient();
         Call call = client.newCall(request);
         Response response = call.execute();
-        System.out.println(response);
+        System.out.println(response.body().string());
 
     }
 }
